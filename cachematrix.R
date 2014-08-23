f160e39ae65973346f92fdcaa2e8606077a9a812
Greg Manzi <greg.manzi@gmail.com>
2014-08-23 10:11:46 -0700

    Updated the notes- testing commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions are used to cache the inverse of a matrix.
 
 ## Write a short comment describing this function
 
